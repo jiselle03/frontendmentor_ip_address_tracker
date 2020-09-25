04081272f23f46e59155578ddc55e26012ecd0bd
Jiselle Liu <jiselle03@gmail.com>
2020-09-25 00:17:40 -0700

    Simplify code with loop

--- src/components/ResultBox.js
+++ src/components/ResultBox.js
@@ -2,36 +2,36 @@ import React from 'react';
 
 const ResultBox = props => {
     const { ip, location, isp } = props.result;
+    const results = [
+        {
+            "label": "IP Address",
+            "data": ip || "N/A"
+        },
+        {
+            "label": "Location",
+            "data": location ? `${location.city}, ${location.region} ${location.postalCode}` : "N/A"
+        },
+        {
+            "label": "Timezone",
+            "data": location ? location.timezone : "N/A"
+        },
+        {
+            "label": "ISP",
+            "data": isp || "N/A"
+        }
+    ];
 
     return (
         <div className="result-container">
-            <div className="result">
-                <small>IP Address</small>
-                {ip && (
-                    <h3>{ip}</h3>
-                )}
-            </div>
-            <div className="vr"></div>
-            <div className="result">
-                <small>Location</small>
-                {location && (
-                    <h3>{location.city}, {location.region} {location.postalCode}</h3>
-                )}
-            </div>
-            <div className="vr"></div>
-            <div className="result">
-                <small>Timezone</small>
-                {location && (
-                    <h3>{location.timezone}</h3>
-                )}
-            </div>
-            <div className="vr"></div>
-            <div className="result">
-                <small>ISP</small>
-                {isp && (
-                    <h3>{isp}</h3>
-                )}
-            </div>
+            {results.map(result => (
+                <>
+                    <div key={result.label} className="result">
+                        <small>{result.label}</small>
+                        <h3>{result.data}</h3>
+                    </div>
+                    <div className="vr"></div>
+                </>
+            ))}
         </div>
     );
 };
