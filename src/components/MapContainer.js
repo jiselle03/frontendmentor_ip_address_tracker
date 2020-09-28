0d903eab2553f5f51d5276b310b07c7661d37b1a
Jiselle Liu <jiselle03@gmail.com>
2020-09-27 22:18:44 -0700

    Create MapContainer component and add map

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -12,9 +12,6 @@ const MapContainer = props => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 />
-                <Marker position={position}>
-                    <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-                </Marker>
             </Map>
         </div>
     );
